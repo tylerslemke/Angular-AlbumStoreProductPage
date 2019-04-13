2ca7d5c1b79e8b6607abc5b952e5db113ea9fdbd
Tyler Lemke <tyler.lemke@allstar.technology>
2019-04-13 18:03:36 -0500

    satisfy the tests

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Album} from './album';
-import {Observable} from "rxjs";
+import {Observable} from "rxjs/Observable";
 @Injectable()
 export class ProductService {
 
